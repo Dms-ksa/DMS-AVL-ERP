e083ec1a5d2aa6c519fe89c656a7e5eee6882ebb
mujtaba <wyrm3535>
2025-05-02 20:39:37 +0000

    Remving AVL installatiojn object types and changing to Installation notes

---
+++ avl_erp/avl_erp/doctype/avl_install/avl_install.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Mujtaba Alsaif and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("AVL Install", {
+// 	refresh(frm) {
+
+// 	},
+// });
